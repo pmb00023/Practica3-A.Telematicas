cc5b525d8097799d086742e0a9abe1e27ed35efb
usuario <usuario@simu19>
2019-03-18 12:14:24 +0100

    Continuación de comentario y uso en casa (con tarjetero)

---
+++ src/ObtenerDatos.java
@@ -0,0 +1,221 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.security.cert.CertificateException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.smartcardio.*;
+
+/**
+ * La clase ObtenerDatos implementa cuatro mÃ©todos pÃºblicos que permiten obtener
+ * determinados datos de los certificados de tarjetas DNIe, Izenpe y Ona.
+ *
+ * @author tbc
+ */
+public class ObtenerDatos {
+
+    private static final byte[] dnie_v_1_0_Atr = {
+        (byte) 0x3B, (byte) 0x7F, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x6A, (byte) 0x44,
+        (byte) 0x4E, (byte) 0x49, (byte) 0x65, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+        (byte) 0x00, (byte) 0x00, (byte) 0x90, (byte) 0x00};
+    private static final byte[] dnie_v_1_0_Mask = {
+        (byte) 0xFF, (byte) 0xFF, (byte) 0x00, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+        (byte) 0x00, (byte) 0x00, (byte) 0xFF, (byte) 0xFF};
+
+    public ObtenerDatos() {
+    }
+/*Metodo para leer DNI
+ * 
+ */
+    public Usuario LeerNIF() {
+
+        Usuario user = null;
+        byte[] datos=null;
+        try {
+            Card c = ConexionTarjeta();
+            //comprobacion de si existe la tarjeta o no
+            if (c == null) {
+                throw new Exception("ACCESO DNIe: No se ha encontrado ninguna tarjeta");
+            }
+            byte[] atr = c.getATR().getBytes();
+            CardChannel ch = c.getBasicChannel();
+
+            if (esDNIe(atr)) {
+            	//Si se pueden leer los datos
+                datos = leerCertificado(ch);
+                if(datos!=null)
+                    
+                	//Metodo que devuelve el objeto con los datos
+                    user = leerDatosUsuario(datos);
+            }
+            c.disconnect(false);
+
+        } catch (Exception ex) {
+            Logger.getLogger(ObtenerDatos.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return user;
+    }
+/*MEtodo para poder leer los datos del DNI(Introduciendo los datos de la tarjeta) teniendo en cuenta
+/las excepciones de transmision y uso del DNIE
+ * 
+ */
+    public byte[] leerCertificado(CardChannel ch) throws CardException, CertificateException {
+
+
+        int offset = 0;
+        String completName = null;
+
+        //[1] PRÃ�CTICA 3. Punto 1.a
+        
+        byte[] command = new byte[]{(byte) 0x00, (byte) 0xa4, (byte) 0x04, (byte) 0x00, (byte) 0x0b, (byte) 0x4D, (byte) 0x61, (byte) 0x73, (byte) 0x74, (byte) 0x65, (byte) 0x72, (byte) 0x2E, (byte) 0x46, (byte) 0x69, (byte) 0x6C, (byte) 0x65};
+        ResponseAPDU r = ch.transmit(new CommandAPDU(command));
+        if ((byte) r.getSW() != (byte) 0x9000) {
+            System.out.println("ACCESO DNIe: SW incorrecto");
+            return null;
+        }
+
+        //[2] PRÃ�CTICA 3. Punto 1.a
+        command = new byte[]{(byte) 0x00, (byte) 0xA4, (byte) 0x00, (byte) 0x00, (byte) 0x02, (byte) 0x50, (byte) 0x15};
+        r = ch.transmit(new CommandAPDU(command));
+
+        if ((byte) r.getSW() != (byte) 0x9000) {
+            System.out.println("ACCESO DNIe: SW incorrecto");
+            return null;
+        }
+
+        //[3] PRÃ�CTICA 3. Punto 1.a
+        command = new byte[]{(byte) 0x00, (byte) 0xA4, (byte) 0x00, (byte) 0x00, (byte) 0x02, (byte) 0x60, (byte) 0x04};
+        r = ch.transmit(new CommandAPDU(command));
+
+        byte[] responseData = null;
+        if ((byte) r.getSW() != (byte) 0x9000) {
+            System.out.println("ACCESO DNIe: SW incorrecto");
+            return null;
+        } else {
+            responseData = r.getData();
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] r2 = null;
+        int bloque = 0;
+
+        do {
+             //[4] PRÃ�CTICA 3. Punto 1.b OJO
+            final byte CLA = (byte) 0xFF;//Buscar quÃ© valor poner aquÃ­ (0xFF no es el correcto)
+            final byte INS = (byte) 0xFF;//Buscar quÃ© valor poner aquÃ­ (0xFF no es el correcto)
+            final byte LE = (byte) 0xFF;// Identificar quÃ© significa este valor
+
+            //[4] PRÃ�CTICA 3. Punto 1.b
+            command = new byte[]{CLA, INS, (byte) bloque/*P1*/, (byte) 0x00/*P2*/, LE};//Identificar quÃ© hacen P1 y P2
+            r = ch.transmit(new CommandAPDU(command));
+
+            //System.out.println("ACCESO DNIe: Response SW1=" + String.format("%X", r.getSW1()) + " SW2=" + String.format("%X", r.getSW2()));
+
+            if ((byte) r.getSW() == (byte) 0x9000) {
+                r2 = r.getData();
+
+                baos.write(r2, 0, r2.length);
+
+                for (int i = 0; i < r2.length; i++) {
+                    byte[] t = new byte[1];
+                    t[0] = r2[i];
+                    System.out.println(i + (0xff * bloque) + String.format(" %2X", r2[i]) + " " + String.format(" %d", r2[i])+" "+new String(t));
+                    
+                }
+                bloque++;
+            } else {
+                return null;
+            }
+
+        } while (r2.length >= 0xfe);
+
+
+         ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+
+      
+
+        
+        return baos.toByteArray();
+    }
+
+    
+    
+    
+    /**
+     * Este mÃ©todo establece la conexiÃ³n con la tarjeta. La funciÃ³n busca el
+     * Terminal que contenga una tarjeta, independientemente del tipo de tarjeta
+     * que sea.
+     *
+     * @return objeto Card con conexiÃ³n establecida
+     * @throws Exception
+     */
+    private Card ConexionTarjeta() throws Exception {
+
+        Card card = null;
+        TerminalFactory factory = TerminalFactory.getDefault();
+        List<CardTerminal> terminals = factory.terminals().list();
+        //System.out.println("Terminals: " + terminals);
+
+        for (int i = 0; i < terminals.size(); i++) {
+
+            // get terminal
+            CardTerminal terminal = terminals.get(i);
+
+            try {
+                if (terminal.isCardPresent()) {
+                    card = terminal.connect("*"); //T=0, T=1 or T=CL(not needed)
+                }
+            } catch (Exception e) {
+
+                System.out.println("Exception catched: " + e.getMessage());
+                card = null;
+            }
+        }
+        return card;
+    }
+
+    /**
+     * Este mÃ©todo nos permite saber el tipo de tarjeta que estamos leyendo del
+     * Terminal, a partir del ATR de Ã©sta.
+     *
+     * @param atrCard ATR de la tarjeta que estamos leyendo
+     * @return tipo de la tarjeta. 1 si es DNIe, 2 si es Starcos y 0 para los
+     * demÃ¡s tipos
+     */
+    private boolean esDNIe(byte[] atrCard) {
+        int j = 0;
+        boolean found = false;
+
+        //Es una tarjeta DNIe?
+        if (atrCard.length == dnie_v_1_0_Atr.length) {
+            found = true;
+            while (j < dnie_v_1_0_Atr.length && found) {
+                if ((atrCard[j] & dnie_v_1_0_Mask[j]) != (dnie_v_1_0_Atr[j] & dnie_v_1_0_Mask[j])) {
+                    found = false; //No es una tarjeta DNIe
+                }
+                j++;
+            }
+        }
+
+        if (found == true) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Analizar los datos leÃ­dos del DNIe para obtener
+     *   - nombre
+     *   - apellidos
+     *   - NIF
+     * @param datos
+     * @return 
+     */
+    private Usuario leerDatosUsuario(byte[] datos) {
+       return null;
+    }
+}
